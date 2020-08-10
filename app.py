58ed6d7d62985257a3f9e09c71b38c85cda7d5e7
Russell Jarvis <rjjarvis@asu.edu>
2020-08-10 17:08:07 +1000

    moved plotting functions to different file

--- app.py
+++ app.py
@@ -23,223 +23,12 @@ except:
     PLOTLY = False
 from tqdm.auto import tqdm
 import seaborn as sns    
-from statsmodels.tsa.stattools import grangercausalitytests
-
+#from statsmodels.tsa.stattools import grangercausalitytests
+import plotting_functions as pf
 # Function: process_fitbit_sleep_data()
 # fileList: A list of fitbit sleep data files eg ["sleep-2020-03-09.json","sleep-2020-04-08.json".....]
 # Returns a dataframe with the following columns:
 # ['duration', 'efficiency', 'endTime', 'mainSleep', 'minutesAfterWakeup', 'minutesAsleep', 'minutesAwake', 'minutesToFallAsleep', 'startTime', 'summary.asleep.count', 'summary.asleep.minutes', 'summary.awake.count', 'summary.awake.minutes', 'summary.deep.count', 'summary.deep.minutes', 'summary.deep.thirtyDayAvgMinutes', 'summary.light.count', 'summary.light.minutes', 'summary.light.thirtyDayAvgMinutes', 'summary.rem.count', 'summary.rem.minutes', 'summary.rem.thirtyDayAvgMinutes', 'summary.restless.count', 'summary.restless.minutes', 'summary.wake.count', 'summary.wake.minutes', 'summary.wake.thirtyDayAvgMinutes', 'timeInBed', 'type', 'dayOfWeek', 'rem.%', 'deep.%', 'wake.%', 'light.%', 'startMin', 'endMin']
-
-
-
-#@st.cache            
-def process_fitbit_sleep_data(fileList):
-    full_sleep_df = None
-    for input_file in fileList:#,title='Loading in fitbit data'):
-        input_df = pd.read_json(input_file)
-        detail_df = json_normalize(input_df['levels'])
-        sleep_df = pd.concat([input_df, detail_df], axis =1)
-        full_sleep_df = pd.concat([full_sleep_df, sleep_df], sort=True)
-
-    full_sleep_df['dateOfSleep']= pd.to_datetime(full_sleep_df['dateOfSleep'])
-    full_sleep_df['dayOfWeek'] = full_sleep_df['dateOfSleep'].dt.day_name()
-    full_sleep_df = full_sleep_df.set_index('dateOfSleep')
-    full_sleep_df.sort_index(inplace=True)
-
-    full_sleep_df['duration'] = full_sleep_df['duration']/(1000*60) # convert duration to minutes
-
-    for col in ['rem','deep','wake','light']:
-        full_sleep_df[col + '.%'] = 100*full_sleep_df['summary.' + col + '.minutes']/full_sleep_df['duration']
-
-    full_sleep_df['startMin'] = pd.to_datetime(full_sleep_df['startTime']).dt.minute + 60 * pd.to_datetime(full_sleep_df['startTime']).dt.hour
-
-    full_sleep_df['startMin'] = np.where(full_sleep_df['startMin'] < 240, full_sleep_df['startMin'] + 1440, full_sleep_df['startMin']) # handle v late nights
-
-    full_sleep_df['endMin'] = pd.to_datetime(full_sleep_df['endTime']).dt.minute + 60 * pd.to_datetime(full_sleep_df['endTime']).dt.hour
-
-    #remove rows which are not mainSleep == True (these are naps not sleeps)
-    full_sleep_df = full_sleep_df[full_sleep_df.mainSleep != False]
-
-    #remove column which are not needed/useful
-    full_sleep_df.drop(['logId', 'data', 'shortData', 'infoCode', 'levels'], axis=1, inplace=True)
-
-    return full_sleep_df
-
-# Function: plot_fitbit_sleep_data()
-# sleep_df: a sleep dataframe
-# cols: the columns to be displayed on the line graph
-# output: saves a line graph to a png file named according to the columns graphed (with vertical lines to indicate weekends)
-def plot_fitbit_sleep_data(sleep_df, cols):
-    sleep_df[cols].plot(figsize=(20,5))
-    plt.title('Sleep plot')
-
-    for date in sleep_df.index[sleep_df['dayOfWeek'] == 'Monday'].tolist():
-        plt.axvline(date)
-
-    output_filename = "sleep"
-    for col in cols:
-        output_filename += "-" + col
-    
-    output_filename += ".png"
-	
-    st.pyplot()
-def plot_fitbit_sleep_data_plotly(sleep_df, cols):
-
-    X = [i for i in range(0,len(sleep_df.index.values))]
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=X, y=sleep_df['rem.%'],
-                        mode='lines',
-                        name='REM sleep'))
-    fig.add_trace(go.Scatter(x=X, y=sleep_df['deep.%'],
-                        mode='lines+markers',
-                        name='deep sleep'))
-    st.write(fig)
-
-# Function: plot_sleep_data_scatter 
-# sleep_df: a sleep dataframe
-# cols: the columns to be displayed on the scatter plot
-# output: saves a scatter plot to a png file named according to the columns graphed
-def plot_sleep_data_scatter(full_sleep_df, col1, col2):
-    full_sleep_df.plot.scatter(x=col1, y=col2)
-    plt.title('Sleep plot')
-    plt.ylabel(col2)
-    plt.xlabel(col1)
-    st.pyplot()
-
-def plot_sleep_data_scatter_plotly(full_sleep_df, col1, col2):
-    fig = px.scatter(x=full_sleep_df[col1], y=full_sleep_df[col2],labels={
-                     "x": col1,
-                     "y": col2                 
-                },
-                title="Sleep plot")
-
-    st.write(fig)
-
-# Function: plot_corr(sleep_df)
-# Function plots a graphical correlation matrix for each pair of columns in the dataframe.
-# sleep_df: pandas DataFrame
-
-def plot_corr(sleep_df):
-
-    f = plt.figure(figsize=(19, 15))
-    fig = sns.heatmap(sleep_df.corr())
-    
-    cols = ['duration', 'efficiency', 'summary.deep.minutes', 'summary.deep.minutes.%', 'summary.light.minutes', 'summary.light.minutes.%', 'summary.rem.minutes', 'summary.rem.minutes.%', 'summary.wake.minutes', 'summary.wake.minutes.%', 'startMin', 'avg4_startMin', 'startTimeDeviation1.%', 'startTimeDeviation4.%']
-    plt.xticks(range(sleep_df.shape[1]), cols, fontsize=12, rotation="vertical")
-    plt.yticks(range(sleep_df.shape[1]), cols, fontsize=12)
-
-    plt.title('Correlation Matrix', fontsize=14)
-    st.pyplot()
-from sklearn.impute import SimpleImputer
-import copy
-def df_to_plotly(df,log=False):
-    return {'z': df.values.tolist(),
-            'x': df.columns.tolist(),
-            'y': df.index.tolist()}
-
-def plot_corr_plotly(sleep_df):
-    sns_colorscale = [[0.0, '#3f7f93'], #cmap = sns.diverging_palette(220, 10, as_cmap = True)
-    [0.071, '#5890a1'],
-    [0.143, '#72a1b0'],
-    [0.214, '#8cb3bf'],
-    [0.286, '#a7c5cf'],
-    [0.357, '#c0d6dd'],
-    [0.429, '#dae8ec'],
-    [0.5, '#f2f2f2'],
-    [0.571, '#f7d7d9'],
-    [0.643, '#f2bcc0'],
-    [0.714, '#eda3a9'],
-    [0.786, '#e8888f'],
-    [0.857, '#e36e76'],
-    [0.929, '#de535e'],
-    [1.0, '#d93a46']]
-    heat = go.Heatmap(df_to_plotly(sleep_df.corr()),colorscale=sns_colorscale)
-    #fig = go.Figure(data=
-
-    title = 'Correlation Matrix'               
-
-    layout = go.Layout(title_text=title, title_x=0.5, 
-                    width=600, height=600,
-                    xaxis_showgrid=False,
-                    yaxis_showgrid=False,
-                    yaxis_autorange='reversed')
-    
-    fig=go.Figure(data=[heat], layout=layout)      
-
-    st.write(fig)
-def plot_df_plotly(sleep_df):
-    fig = go.Figure(data=go.Heatmap(df_to_plotly(sleep_df,log=True)))
-    st.write(fig)
-
-
-def crosscorr(datax, datay, lag=0, wrap=False):
-    """ Lag-N cross correlation. 
-    Shifted data filled with NaNs 
-    Next :
-    https://stackoverflow.com/questions/33171413/cross-correlation-time-lag-correlation-with-pandas
-    Parameters
-    ----------
-    lag : int, default 0
-    datax, datay : pandas.Series objects of equal length
-    Returns
-    ----------
-    crosscorr : float
-    """
-    if wrap:
-        shiftedy = datay.shift(lag)
-        shiftedy.iloc[:lag] = datay.iloc[-lag:].values
-        return datax.corr(shiftedy)
-    else: 
-        return datax.corr(datay.shift(lag))
-def check_time_lags(df,col0,col1):
-    sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})
-    sns.set_context("paper", font_scale=3.9)                                                  
-    #plt.figure(figsize=(3.1, 3)) # Two column paper. Each column is about 3.15 inch wide.                                                                                                                                                                                                                                 
-    #color = sns.color_palette("Set2", 6)
-    # https://towardsdatascience.com/granger-causality-and-vector-auto-regressive-model-for-time-series-forecasting-3226a64889a6
-    d1 = df[col0]
-    d2 = df[col1]
-    hours = 5
-    days = 30
-    #https://gist.github.com/jcheong0428/7d5759f78145fc0dc979337f82c6ea33
-    #seconds = 5
-    #fps = 30
-    rs = [crosscorr(d1,d2, lag) for lag in range(-int(hours*days),int(hours*days+1))]
-    offset = np.ceil(len(rs)/2)-np.argmax(rs)
-    f,ax=plt.subplots(figsize=(20,13))
-    re_title = 'look for time lags when correlation is maximised (peak synchrony) betwen {0} and {1}'.format(col0,col1)
-    ax.plot(rs)
-    ax.axvline(np.ceil(len(rs)/2),color='k',linestyle='--',label='Center')
-    ax.axvline(np.argmax(rs),color='r',linestyle='--',label='Peak synchrony')
-    ax.set(title=f'Offset = {offset} frames\nS1 leads <> S2 leads'+re_title,ylim=[.1,.31],xlim=[0,301], xlabel='Offset',ylabel='Pearson r')
-    ax.set_xticks([0, 50, 100, 151, 201, 251, 301])
-    ax.set_xticklabels([-150, -100, -50, 0, 50, 100, 150]);
-    plt.legend()  
-    st.pyplot()
-    #f,ax=plt.subplots(figsize=(20,13))
-    #plt.xcorr([np.abs(d) for d in d1], [np.abs(d) for d in d2], normed=True, usevlines=True, maxlags=10)
-
-    #st.pyplot()
-
-#import statsmodels.api as sm
-#from statsmodels.tsa.api import VAR
-#def analyze_coherance(df,col0,col1):
-#    d1 = df[col0]
-#    d2 = df[col1]
-    #plt.specgram(signalData,Fs=1)
-
-
-    #plt.cohere(d1,d2,NFFT=len(d1)/2)  
-    # import for Granger's Causality Test
-#    granger_test = sm.tsa.stattools.grangercausalitytests(df, maxlag=2, verbose=True)
-#    st.text(granger_test)
-#    df_differenced = df.diff().dropna()
-
-##    model = VAR(df_differenced)
-#    results = model.fit(maxlags=15, ic='aic')
-#    st.text(results.summary())
-    #st.pyplot()
-
 
 
 if __name__ == "__main__":  
@@ -249,24 +38,13 @@ if __name__ == "__main__":
     st.markdown('''
     This is a markdown string that explains sleep data from date {0}
     '''.format(str('2020-03-09')))
-    sleep_df = process_fitbit_sleep_data(fileList)
-        #st.write(df1)
-    df = copy.copy(sleep_df)
- 
-    del df['endTime']
-    del df['dayOfWeek']
-    del df['startTime']
-    del df['type']
-    #imp = SimpleImputer(strategy="most_frequent")
-    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-    df.values[:] = imp.fit_transform(df.values[:])
+    sleep_df = pf.process_fitbit_sleep_data(fileList)
 
-    #st.write(sleep_df, unsafe_allow_html=True)
+
     if PLOTLY:
-        plot_fitbit_sleep_data_plotly(sleep_df, ['rem.%', 'deep.%'])
-        plot_sleep_data_scatter_plotly(sleep_df, 'startMin', 'deep.%')
-        plot_corr_plotly(sleep_df)
-        check_time_lags(sleep_df,'rem.%','deep.%')
+
+        pf.plot_fitbit_sleep_data_plotly(sleep_df, ['rem.%', 'deep.%'])
+        pf.plot_sleep_data_scatter_plotly(sleep_df, 'startMin', 'deep.%')
 
         '''
         If imputation worked there would be no nans visible below:
@@ -274,13 +52,25 @@ if __name__ == "__main__":
         fill with zero,
         impute zeros with mean
         '''
-        plot_df_plotly(sleep_df)#,'rem.%','deep.%')
-     
+        #pf.plot_df_plotly(sleep_df)#,'rem.%','deep.%')
+        pf.plot_corr(sleep_df)
+
+        pf.plot_corr_plotly(sleep_df)
+
+        #NON_DER = ['startMin',]
+        #ts_sleep_df = pf.df_derived_by_shift(sleep_df,1,NON_DER)
+        #pf.plot_corr(ts_sleep_df)
+        #pf.covariance_matrix(sleep_df)
+
+        pf.check_time_lags(sleep_df,'rem.%','deep.%')
+        
+
+
 
     else:
-        plot_corr(sleep_df)
-        plot_fitbit_sleep_data(sleep_df, ['rem.%', 'deep.%'])
-        plot_sleep_data_scatter(sleep_df, 'startMin', 'deep.%')
+        pf.plot_corr(sleep_df)
+        pf.plot_fitbit_sleep_data(sleep_df, ['rem.%', 'deep.%'])
+        pf.plot_sleep_data_scatter(sleep_df, 'startMin', 'deep.%')
 
 
 
---
+++ plotting_functions.py
@@ -0,0 +1,286 @@
+"""
+Process-sleep-data.py
+Processes fitbit sleep data files, uploads data into fitbit dataframe, outputs graphs.
+"""  	
+
+import pandas as pd
+import numpy as np
+import datetime as dt  
+import matplotlib.pyplot as plt  
+from datetime import datetime                          
+import streamlit as st
+try: 
+    json_normalize = pd.json_normalize
+except:
+    from pandas.io.json import json_normalize
+
+try:
+    import plotly.express as px
+    import plotly.graph_objects as go # or plotly.express as px
+
+    PLOTLY = True
+except:
+    PLOTLY = False
+from tqdm.auto import tqdm
+import seaborn as sns    
+from statsmodels.tsa.stattools import grangercausalitytests
+from sklearn.impute import SimpleImputer
+import copy
+# Function: process_fitbit_sleep_data()
+# fileList: A list of fitbit sleep data files eg ["sleep-2020-03-09.json","sleep-2020-04-08.json".....]
+# Returns a dataframe with the following columns:
+# ['duration', 'efficiency', 'endTime', 'mainSleep', 'minutesAfterWakeup', 'minutesAsleep', 'minutesAwake', 'minutesToFallAsleep', 'startTime', 'summary.asleep.count', 'summary.asleep.minutes', 'summary.awake.count', 'summary.awake.minutes', 'summary.deep.count', 'summary.deep.minutes', 'summary.deep.thirtyDayAvgMinutes', 'summary.light.count', 'summary.light.minutes', 'summary.light.thirtyDayAvgMinutes', 'summary.rem.count', 'summary.rem.minutes', 'summary.rem.thirtyDayAvgMinutes', 'summary.restless.count', 'summary.restless.minutes', 'summary.wake.count', 'summary.wake.minutes', 'summary.wake.thirtyDayAvgMinutes', 'timeInBed', 'type', 'dayOfWeek', 'rem.%', 'deep.%', 'wake.%', 'light.%', 'startMin', 'endMin']
+
+sns_colorscale = [[0.0, '#3f7f93'], #cmap = sns.diverging_palette(220, 10, as_cmap = True)
+[0.071, '#5890a1'],
+[0.143, '#72a1b0'],
+[0.214, '#8cb3bf'],
+[0.286, '#a7c5cf'],
+[0.357, '#c0d6dd'],
+[0.429, '#dae8ec'],
+[0.5, '#f2f2f2'],
+[0.571, '#f7d7d9'],
+[0.643, '#f2bcc0'],
+[0.714, '#eda3a9'],
+[0.786, '#e8888f'],
+[0.857, '#e36e76'],
+[0.929, '#de535e'],
+[1.0, '#d93a46']]
+
+@st.cache            
+def process_fitbit_sleep_data(fileList):
+    full_sleep_df = None
+    for input_file in fileList:#,title='Loading in fitbit data'):
+        input_df = pd.read_json(input_file)
+        detail_df = json_normalize(input_df['levels'])
+        sleep_df = pd.concat([input_df, detail_df], axis =1)
+        full_sleep_df = pd.concat([full_sleep_df, sleep_df], sort=True)
+
+    full_sleep_df['dateOfSleep']= pd.to_datetime(full_sleep_df['dateOfSleep'])
+    full_sleep_df['dayOfWeek'] = full_sleep_df['dateOfSleep'].dt.day_name()
+    full_sleep_df = full_sleep_df.set_index('dateOfSleep')
+    full_sleep_df.sort_index(inplace=True)
+
+    full_sleep_df['duration'] = full_sleep_df['duration']/(1000*60) # convert duration to minutes
+
+    for col in ['rem','deep','wake','light']:
+        full_sleep_df[col + '.%'] = 100*full_sleep_df['summary.' + col + '.minutes']/full_sleep_df['duration']
+
+    full_sleep_df['startMin'] = pd.to_datetime(full_sleep_df['startTime']).dt.minute + 60 * pd.to_datetime(full_sleep_df['startTime']).dt.hour
+
+    full_sleep_df['startMin'] = np.where(full_sleep_df['startMin'] < 240, full_sleep_df['startMin'] + 1440, full_sleep_df['startMin']) # handle v late nights
+
+    full_sleep_df['endMin'] = pd.to_datetime(full_sleep_df['endTime']).dt.minute + 60 * pd.to_datetime(full_sleep_df['endTime']).dt.hour
+
+    #remove rows which are not mainSleep == True (these are naps not sleeps)
+    full_sleep_df = full_sleep_df[full_sleep_df.mainSleep != False]
+
+    #remove column which are not needed/useful
+    full_sleep_df.drop(['logId', 'data', 'shortData', 'infoCode', 'levels'], axis=1, inplace=True)
+
+    return full_sleep_df
+
+
+def covariance_matrix(df):
+    # Covariance
+    X = df.values[:]
+    # Calculate covariance matrix 
+    cov_matrix = numpy.cov((X.T) # (or with np.cov(X.T))
+    colormap = plt.cm.RdBu
+
+    svm = sns.heatmap(cov_matrix,
+                square=True,linecolor='white')    
+    cols = ['duration', 'efficiency', 'summary.deep.minutes', 'summary.deep.minutes.%', 'summary.light.minutes', 'summary.light.minutes.%', 'summary.rem.minutes', 'summary.rem.minutes.%', 'summary.wake.minutes', 'summary.wake.minutes.%', 'startMin', 'avg4_startMin', 'startTimeDeviation1.%', 'startTimeDeviation4.%']
+    plt.xticks(range(df.shape[1]), cols, fontsize=12, rotation="vertical")
+    plt.yticks(range(df.shape[1]), cols, fontsize=12)
+
+    plt.title('Covariance Matrix', fontsize=14)
+    st.pyplot()
+
+@st.cache
+def df_derived_by_shift(df,lag=0,NON_DER=[]):
+    # https://www.kaggle.com/dedecu/cross-correlation-time-lag-with-pandas
+    df = df.copy()
+    if not lag:
+        return df
+    cols ={}
+    for i in range(1,lag+1):
+        for x in list(df.columns):
+            if x not in NON_DER:
+                if not x in cols:
+                    cols[x] = ['{}_{}'.format(x, i)]
+                else:
+                    cols[x].append('{}_{}'.format(x, i))
+    for k,v in cols.items():
+        columns = v
+        dfn = pd.DataFrame(data=None, columns=columns, index=df.index)    
+        i = 1
+        for c in columns:
+            dfn[c] = df[k].shift(periods=i)
+            i+=1
+        df = pd.concat([df, dfn], axis=1, join_axes=[df.index])
+    return df
+
+
+
+def try_to_impute(sleep_df):
+    df = copy.copy(sleep_df)
+    del df['endTime']
+    del df['dayOfWeek']
+    del df['startTime']
+    del df['type']
+    df = df.dropna()
+    #imp = SimpleImputer(strategy="most_frequent")
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    df.values[:] = imp.fit_transform(df.values[:])
+    return df
+
+# Function: plot_fitbit_sleep_data()
+# sleep_df: a sleep dataframe
+# cols: the columns to be displayed on the line graph
+# output: saves a line graph to a png file named according to the columns graphed (with vertical lines to indicate weekends)
+def plot_fitbit_sleep_data(sleep_df, cols):
+    sleep_df[cols].plot(figsize=(20,5))
+    plt.title('Sleep plot')
+
+    for date in sleep_df.index[sleep_df['dayOfWeek'] == 'Monday'].tolist():
+        plt.axvline(date)
+
+    output_filename = "sleep"
+    for col in cols:
+        output_filename += "-" + col
+    
+    output_filename += ".png"
+	
+    st.pyplot()
+def plot_fitbit_sleep_data_plotly(sleep_df, cols):
+
+    X = [i for i in range(0,len(sleep_df.index.values))]
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=X, y=sleep_df['rem.%'],
+                        mode='lines',
+                        name='REM sleep'))
+    fig.add_trace(go.Scatter(x=X, y=sleep_df['deep.%'],
+                        mode='lines+markers',
+                        name='deep sleep'))
+    st.write(fig)
+
+# Function: plot_sleep_data_scatter 
+# sleep_df: a sleep dataframe
+# cols: the columns to be displayed on the scatter plot
+# output: saves a scatter plot to a png file named according to the columns graphed
+def plot_sleep_data_scatter(full_sleep_df, col1, col2):
+    full_sleep_df.plot.scatter(x=col1, y=col2)
+    plt.title('Sleep plot')
+    plt.ylabel(col2)
+    plt.xlabel(col1)
+    st.pyplot()
+
+def plot_sleep_data_scatter_plotly(full_sleep_df, col1, col2):
+    fig = px.scatter(x=full_sleep_df[col1], y=full_sleep_df[col2],labels={
+                     "x": col1,
+                     "y": col2                 
+                },
+                title="Sleep plot")
+
+    st.write(fig)
+
+# Function: plot_corr(sleep_df)
+# Function plots a graphical correlation matrix for each pair of columns in the dataframe.
+# sleep_df: pandas DataFrame
+
+def plot_corr(sleep_df,lag=None):
+
+    f = plt.figure(figsize=(19, 15))
+
+    colormap = plt.cm.RdBu
+    plt.figure(figsize=(15,10))
+    plt.title(u'no lag yet', y=1.05, size=16)
+    mask = np.zeros_like(sleep_df.corr())
+    mask[np.triu_indices_from(mask)] = True
+    fig = sns.heatmap(sleep_df.corr(),mask=mask,
+        linewidths=0.1,square=True, linecolor='white')
+    cols = ['duration', 'efficiency', 
+    'summary.deep.minutes', 'summary.deep.minutes.%', 
+    'summary.light.minutes', 'summary.light.minutes.%', 
+    'summary.rem.minutes', 'summary.rem.minutes.%', 
+    'summary.wake.minutes', 'summary.wake.minutes.%',
+     'startMin', 'avg4_startMin', 
+     'startTimeDeviation1.%', 'startTimeDeviation4.%']
+    plt.xticks(range(sleep_df.shape[1]), cols, fontsize=12, rotation="vertical")
+    plt.yticks(range(sleep_df.shape[1]), cols, fontsize=12)
+    plt.title('Correlation Matrix')#, fontsize=14)
+    st.pyplot()
+
+def df_to_plotly(df,log=False):
+    return {'z': df.values.tolist(),
+            'x': df.columns.tolist(),
+            'y': df.index.tolist()}
+def plot_corr_plotly(sleep_df):
+
+    heat = go.Heatmap(df_to_plotly(sleep_df.corr()),colorscale=sns_colorscale)
+    #fig = go.Figure(data=
+
+    title = 'Correlation Matrix'               
+
+    layout = go.Layout(title_text=title, title_x=0.5, 
+                    width=600, height=600,
+                    xaxis_showgrid=False,
+                    yaxis_showgrid=False,
+                    yaxis_autorange='reversed')
+    
+    fig=go.Figure(data=[heat], layout=layout)      
+
+    st.write(fig)
+
+def plot_df_plotly(sleep_df):
+    fig = go.Figure(data=go.Heatmap(df_to_plotly(sleep_df,log=True)))
+    st.write(fig)
+
+
+def crosscorr(datax, datay, lag=0, wrap=False):
+    """ Lag-N cross correlation. 
+    Shifted data filled with NaNs 
+    Next :
+    https://stackoverflow.com/questions/33171413/cross-correlation-time-lag-correlation-with-pandas
+    Parameters
+    ----------
+    lag : int, default 0
+    datax, datay : pandas.Series objects of equal length
+    Returns
+    ----------
+    crosscorr : float
+    """
+    if wrap:
+        shiftedy = datay.shift(lag)
+        shiftedy.iloc[:lag] = datay.iloc[-lag:].values
+        return datax.corr(shiftedy)
+    else: 
+        return datax.corr(datay.shift(lag))
+def check_time_lags(df,col0,col1):
+    sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})
+    sns.set_context("paper", font_scale=3.9)                                                  
+    #plt.figure(figsize=(3.1, 3)) # Two column paper. Each column is about 3.15 inch wide.                                                                                                                                                                                                                                 
+    #color = sns.color_palette("Set2", 6)
+    # https://towardsdatascience.com/granger-causality-and-vector-auto-regressive-model-for-time-series-forecasting-3226a64889a6
+    d1 = df[col0]
+    d2 = df[col1]
+    hours = 5
+    days = 30
+    #https://gist.github.com/jcheong0428/7d5759f78145fc0dc979337f82c6ea33
+    #seconds = 5
+    #fps = 30
+    rs = [crosscorr(d1,d2, lag) for lag in range(-int(hours*days),int(hours*days+1))]
+    offset = np.ceil(len(rs)/2)-np.argmax(rs)
+    f,ax=plt.subplots(figsize=(20,13))
+    re_title = 'look for time lags when correlation is maximised (peak synchrony) betwen {0} and {1}'.format(col0,col1)
+    ax.plot(rs)
+    ax.axvline(np.ceil(len(rs)/2),color='k',linestyle='--',label='Center')
+    ax.axvline(np.argmax(rs),color='r',linestyle='--',label='Peak synchrony')
+    ax.set(title=f'Offset = {offset} frames\nS1 leads <> S2 leads'+re_title,ylim=[.1,.31],xlim=[0,301], xlabel='Offset',ylabel='Pearson r')
+    ax.set_xticks([0, 50, 100, 151, 201, 251, 301])
+    ax.set_xticklabels([-150, -100, -50, 0, 50, 100, 150]);
+    plt.legend()  
+    st.pyplot()
+
+
