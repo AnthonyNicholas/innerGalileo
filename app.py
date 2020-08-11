a5adbc54eb39327934a57b9ffc9d71fb9b2dda80
Russell Jarvis <colouredstatic@gmail.com>
2020-08-11 15:14:25 +1000

    end

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ Processes fitbit sleep data files, uploads data into fitbit dataframe, outputs g
 import pandas as pd
 import numpy as np
 import datetime as dt  
-import matplotlib.pyplot as plt  
-from datetime import datetime                          
+#import matplotlib.pyplot as plt  
+#from datetime import datetime                          
 import streamlit as st
-try: 
-    json_normalize = pd.json_normalize
-except:
-    from pandas.io.json import json_normalize
+#try: 
+#    json_normalize = pd.json_normalize
+#except:
+#    from pandas.io.json import json_normalize
 
 try:
     import plotly.express as px
@@ -21,34 +21,34 @@ try:
     PLOTLY = True
 except:
     PLOTLY = False
-from tqdm.auto import tqdm
-import seaborn as sns    
-#from statsmodels.tsa.stattools import grangercausalitytests
+#from tqdm.auto import tqdm
+#import seaborn as sns    
 import plotting_functions as pf
-# Function: process_fitbit_sleep_data()
-# fileList: A list of fitbit sleep data files eg ["sleep-2020-03-09.json","sleep-2020-04-08.json".....]
-# Returns a dataframe with the following columns:
-# ['duration', 'efficiency', 'endTime', 'mainSleep', 'minutesAfterWakeup', 'minutesAsleep', 'minutesAwake', 'minutesToFallAsleep', 'startTime', 'summary.asleep.count', 'summary.asleep.minutes', 'summary.awake.count', 'summary.awake.minutes', 'summary.deep.count', 'summary.deep.minutes', 'summary.deep.thirtyDayAvgMinutes', 'summary.light.count', 'summary.light.minutes', 'summary.light.thirtyDayAvgMinutes', 'summary.rem.count', 'summary.rem.minutes', 'summary.rem.thirtyDayAvgMinutes', 'summary.restless.count', 'summary.restless.minutes', 'summary.wake.count', 'summary.wake.minutes', 'summary.wake.thirtyDayAvgMinutes', 'timeInBed', 'type', 'dayOfWeek', 'rem.%', 'deep.%', 'wake.%', 'light.%', 'startMin', 'endMin']
-
+CACHED = True
 if __name__ == "__main__":  
     st.title('Analysis for sleep quality')
-    fileList = ["sleep-2020-03-09.json","sleep-2020-04-08.json","sleep-2020-05-08.json","sleep-2020-06-07.json","sleep-2020-07-07.json"]
+    if CACHED:
+        fileList = ["sleep-2020-03-09.json","sleep-2020-04-08.json","sleep-2020-05-08.json","sleep-2020-06-07.json","sleep-2020-07-07.json"]
 
-    st.markdown('''
-    This is a markdown string that explains sleep data fro
-    m date {0}
-    '''.format(str('2020-03-09')))
+        st.markdown('''
+        This is a markdown string that explains sleep data fro
+        m date {0}
+        '''.format(str('2020-03-09')))
+    else:
+        print('add methods for user upload data')
     sleep_df = pf.process_fitbit_sleep_data(fileList)
 
 
     if PLOTLY:
         st.markdown('''
+        # Using cached data for splash screen
         There are {0} Nan values
         # To see nan's
         Mouse over 
         It looks like some whole columns are NaN and there are a few with just two or so samples.
         We should drop nan columns.
         '''.format(np.sum(sleep_df.isnull().sum().values)))
+        st.markdown('''---''')
 
         pf.plot_df_plotly(sleep_df)#,'rem.%','deep.%')
         sleep_df.dropna(axis=1, how='any',inplace=True, thresh=4)
@@ -64,20 +64,34 @@ if __name__ == "__main__":
         # After drop nan
         that  didn't work perfectly
         '''
+        st.markdown('''---''')
+        st.markdown('''\n\n''')
 
         pf.plot_df_plotly(sleep_df)#,'rem.%','deep.%')
+        st.markdown('''---''')
+        st.markdown('''\n\n''')
+
         st.write(sleep_df.describe())
-        #st.write(sleep_df.dtypes.T)
+        st.markdown('''---''')
         '''
         clustergram useful for exploration
         '''
         sleep_df.dropna(axis=0, how='any',inplace=True, thresh=4)
 
         pf.cluster_map(sleep_df)
+        st.markdown('''---''')
+        st.markdown('''\n\n''')
+
         pf.plot_fitbit_sleep_data_plotly(sleep_df, ['rem.%', 'deep.%'])
+        st.markdown('''---''')
+        st.markdown('''\n\n''')
+
         pf.plot_sleep_data_scatter_plotly(sleep_df, 'startMin', 'deep.%')
+        st.markdown('''---''')
+
         '''
-        hexbins show density of data better:
+        # hexbins, 
+        joint plot show density of data better:
         '''
         pf.plot_sleep_data_joint(sleep_df, 'startMin', 'deep.%')
     
@@ -87,12 +101,16 @@ if __name__ == "__main__":
         fill with zero,
         impute zeros with mean
         '''
+        st.markdown('''---''')
+
 
         pf.plot_corr(sleep_df)
         ''' 
         This is plotly correlation matrix this can be combined with
         seaborn approach and masking
         '''
+        st.markdown('''---''')
+
         pf.plot_imshow_plotly(sleep_df.corr())
         '''
         Covariance matrix backbone of PCA, more useful when we have more features
@@ -100,9 +118,14 @@ if __name__ == "__main__":
         big changes that correlate with small changes (sometimes negatively correlations)
 
         '''
+        st.markdown('''---''')
+
         pf.covariance_matrix(sleep_df)
+        st.markdown('''---''')
         pf.plot_imshow_plotly(sleep_df.cov())
+        st.markdown('''---''')
         pf.check_time_lags(sleep_df, 'rem.%', 'deep.%')
+        st.markdown('''---''')
         pf.check_time_lags(sleep_df, 'startMin', 'deep.%')
 
 
