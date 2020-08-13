1c9d897a00635c595d554a35a230365180e10995
Russell Jarvis <rjjarvis@asu.edu>
2020-08-13 15:28:58 +1000

    load in huge data set, convert to data frame

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ def process_fitbit_sleep_data(fileList):
     full_sleep_df = None
     cnt = 0
     #tqdm(follow_links,title='Scrape in Progress. Please Wait.')
-    for input_file in tqdm(fileList,title='Loading in fitbit data'):
+    for input_file in tqdm(fileList,title='Loading in short interval of sleep fitbit data'):
         input_df = pd.read_json(input_file)
         detail_df = json_normalize(input_df['levels'])
         sleep_df = pd.concat([input_df, detail_df], axis =1)
@@ -72,48 +72,30 @@ def process_fitbit_sleep_data(fileList):
     full_sleep_df.drop(['logId', 'data', 'shortData', 'infoCode', 'levels'], axis=1, inplace=True)
 
     return full_sleep_df
-
+'''
+import dask
+def dask_map_function(eval_,invalid_ind):
+    results = []
+    for x in invalid_ind:
+        y = dask.delayed(eval_)(x)
+        results.append(y)
+    fitnesses = dask.compute(*results)
+    return fitnesses
+'''
 def process_fitbit_every_data(fileList):
     df = None
     cnt = 0
-    for input_file in tqdm(fileList,title='Loading in fitbit data'):
+    for input_file in tqdm(fileList,title='Loading in massive everything data set'):
         input_df = pd.read_json(input_file)
 
         if cnt>0:
             df = pd.concat([df, input_df], axis =1)
         else:
             df = input_df
-        #print(df.shape)
+        
+        #st.write(reduced_df.describe())
         progress_bar.progress(cnt/len(fileList))
         status_text.text("Data Reading %i%% Complete" % float(cnt/len(fileList)))    
         cnt+=1
-
-    if df is not None:
-        df.sort_index(inplace=True)
-    try:
-        df['dateOfSleep']= pd.to_datetime(df['dateOfSleep'])
-        df['dayOfWeek'] = df['dateOfSleep'].dt.day_name()
-        df = df.set_index('dateOfSleep')
-        df.sort_index(inplace=True)
-
-        df['duration'] = df['duration']/(1000*60) # convert duration to minutes
-
-        for col in ['rem','deep','wake','light']:
-            df[col + '.%'] = 100*df['summary.' + col + '.minutes']/df['duration']
-
-        df['startMin'] = pd.to_datetime(df['startTime']).dt.minute + 60 * pd.to_datetime(df['startTime']).dt.hour
-
-        df['startMin'] = np.where(df['startMin'] < 240, full_sleep_df['startMin'] + 1440, df['startMin']) # handle v late nights
-
-        df['endMin'] = pd.to_datetime(df['endTime']).dt.minute + 60 * pd.to_datetime(df['endTime']).dt.hour
-
-        #remove rows which are not mainSleep == True (these are naps not sleeps)
-        df = df[df.mainSleep != False]
-    except:
-        pass
-    try:
-    #remove column which are not needed/useful
-        df.drop(['logId', 'data', 'shortData', 'infoCode', 'levels'], axis=1, inplace=True)
-    except:
-        pass
+    #st.write(df.describe())
     return df
